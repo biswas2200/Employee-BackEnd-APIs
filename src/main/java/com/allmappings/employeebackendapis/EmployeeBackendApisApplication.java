8407d46e7fc2f1d8a15de78854f9247a04fd3d97
Biswas <biswasamitosh2200@gmail.com>
2024-02-09 14:49:50 +0530

    First Commit

---
+++ src/main/java/com/allmappings/employeebackendapis/EmployeeBackendApisApplication.java
@@ -0,0 +1,13 @@
+package com.allmappings.employeebackendapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBackendApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBackendApisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allmappings/employeebackendapis/EmployeeBackendApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allmappings.employeebackendapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBackendApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
